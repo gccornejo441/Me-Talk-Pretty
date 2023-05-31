5044f46c9c32fc19be2d4e6eb8bcdb105fc74665
Gabriel Cornejo <gcornejo441@gmail.com>
2023-05-30 17:02:31 -0700

    changes made to icons

--- background.js
+++ background.js
@@ -1,18 +1,11 @@
-chrome.action.onClicked.addListener((tab) => {
-  chrome.scripting.executeScript({
-    target: {tabId: tab.id},
-    files: ['scripts/content-script.js']
-  });
-});
-
-chrome.runtime.onMessage.addListener( // this is the message listener
-    function(request, sender, sendResponse) {
-      console.log(`****** request message: ${request}`)
-            runThisFunction(request);
-    }
-);
-
-function runThisFunction(msg) {
-  console.log("**********MESSAGED RECEIVED!")
-  console.log(`**************Here is your message ${msg}`)
+try {
+  importScripts(
+    "js/defaults.js",
+    "js/messaging.js",
+    "js/content-handlers.js",
+    "js/events.js"
+  )
+}
+catch (err) {
+  console.error(err)
 }
