acfa9298a496aa2fb6f93ed93e49fefea56a674b
Gabriel Cornejo <gcornejo441@gmail.com>
2023-06-04 14:08:27 -0700

    adding context menu click button

--- src/background.js
+++ src/background.js
@@ -7,18 +7,45 @@ async function copyPage() {
   // chrome.tabs.sendMessage(tab.id, { greeting: "hello" });
 }
 
+function backgroundSendAssets(text, url) {
+  console.log(`***************tab: ${text}`);
+  chrome.tabs.query(
+    { active: true, currentWindow: true },
+    function (tabs) {
+      var tabId = tabs[0].id;
+      chrome.tabs.sendMessage(
+        tabId,
+        { text: text, url: url },
+        function (response) {
+          // Handle the response from the content script
+          console.log("Response from content script:", response);
+
+          // Perform actions based on the response
+          if (response && response.success) {
+            // Handle successful response
+          } else {
+            // Handle unsuccessful response or error
+          }
+        }
+      );
+    }
+  );
+}
+
 function onClickHandler(target, tab) {
-  console.log(`***************message.target: ${JSON.stringify(target)}`);
-  console.log(`***************message.tab: ${JSON.stringify(tab)}`);
+  console.log(`***************lastRequest.action:, ${JSON.stringify(lastRequest)}`);
+
   switch (lastRequest.action) {
     case "copyPage":
       console.log("Im inside of copypage line: 13");
       copyPage();
       break;
+
     case "copyElement":
       var url = target.linkUrl;
       var text = target.selectionText || lastRequest.text;
       console.log(`***************message.TEXT: ${JSON.stringify(text)}`);
+      console.log(`***************message.url: ${!url}`);
 
       if (!url && !text && !target.linkUrl) {
         copyPage();
@@ -26,57 +53,62 @@ function onClickHandler(target, tab) {
       }
 
       if (!target.linkUrl) {
-        chrome.tabs.query(
-          { active: true, currentWindow: true },
-          function (tabs) {
-            var tabId = tabs[0].id;
-            chrome.tabs.sendMessage(
-              tabId,
-              { text: text, url: url },
-              function (response) {
-                // Handle the response from the content script
-                console.log("Response from content script:", response);
-
-                // Perform actions based on the response
-                if (response && response.success) {
-                  // Handle successful response
-                } else {
-                  // Handle unsuccessful response or error
-                }
-              }
-            );
-          }
-        );
+        copyPage();
         break;
       }
 
       text = "![](" + target.linkUrl + ")";
+      console.log(`***************message.TEXT2: ${JSON.stringify(text)}`);
+
       if (url) {
-        copy("[" + text + "](" + url + ")");
+        backgroundSendAssets(text, url)
       } else {
-        copy(text);
+        backgroundSendAssets(text)
       }
       break;
+
     case "copySelection":
       var str = "";
       var links = lastRequest.links;
+
       for (var i = 0; i < links.length; i++) {
+      console.log(`***************message.links at 70: ${JSON.stringify(links)}`);
+
         var link = links[i];
         var text = link.text;
+
         if (link.src) {
           text = "![](" + link.src + ")";
         }
+
         str += "- [" + text + "](" + (link.href || "") + ")\n";
       }
-      copy(str);
+
+      backgroundSendAssets(str)
       break;
+
   }
 }
 
 chrome.contextMenus.onClicked.addListener(function (info, tab) {
   if (info.menuItemId === "menuOption") {
-    console.log("The Context menu has been clicked");
-    onClickHandler(info, tab);
+
+    chrome.tabs.query(
+      { active: true, currentWindow: true },
+      function (tabs) {
+        var tabId = tabs[0].id;
+        chrome.tabs.sendMessage(
+          tabId,
+          { action: info.menuItemId },
+          function (response) {
+            // Handle the response from the content script
+            console.log("Response from content script:", response);
+
+          }
+        );
+      }
+    );
+    // onClickHandler(info, tab);
   }
 });
 
--- src/content_script.js
+++ src/content_script.js
@@ -9,12 +9,63 @@
   
   
 })()
-var copy = function(text) {
 
-  console.log(`***************: ${text} is ZERO`)
-  $('#clipboard_area').text("Hi this is static text")[0].select();
-  document.execCommand('Copy');
-}
+
+
+var copy = function(text) {
+  var value = $('#clipboard_area').text(text)
+  console.log(`*************** copy from value: ${value}`)
+
+    $('#clipboard_area').text(text)[0].select();
+    document.execCommand('Copy');
+  }
+
+  function gatherAssets() {
+    document.addEventListener("contextmenu", async function (event) {
+      var selection = window.getSelection();
+    
+      var linksInSelection = 0;
+      var text;
+      if (selection.rangeCount == 0) {
+        console.log(`***************: ${selection.rangeCount} is ZERO`)
+        text = $(event.target).text() || $(event.target).parent().text()
+      } else {
+        console.log(`***************selection.rangeCount is: ${selection.rangeCount}`)
+        var clonedSelection = selection.getRangeAt(0).cloneContents();
+        var div = document.createElement('div');
+        div.appendChild(clonedSelection);
+        linksInSelection = $('a[href]', div);
+      }
+    
+      if (linksInSelection.length > 0) {
+        console.log(`***************LinksInSelection: ${linksInSelection.length}`)
+    
+        links = [];
+        linksInSelection.each(function() {
+          links.push({
+            href: $(this).prop('href'),
+            text: $(this).text(),
+            src:  $(this).find('img').attr('src')
+          });
+        });
+        console.log("here")
+        await chrome.runtime.sendMessage({ action: "copySelection", links: links });
+      } else {
+        if (text) {
+          await chrome.runtime.sendMessage({ action: "copyElement", text: text });
+        } else {
+          console.log(`************ No Text: ${text}`)
+        }
+      }
+    }, true);
+  }
+
+  chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
+    console.log(`*******************Listening for context menu:  ${message.action}`)
+    if (message.action === "menuOption") {
+      gatherAssets()
+    } 
+  });
 
 window.addEventListener('keydown', function(event) {
   // Command + Ctrl + C
@@ -22,57 +73,28 @@ window.addEventListener('keydown', function(event) {
   chrome.runtime.sendMessage({ action: "copyPage" });
 }, true);
 
-document.addEventListener("contextmenu", async function (event) {
-  var selection = window.getSelection();
-
-  var linksInSelection = 0;
-  var text;
-  if (selection.rangeCount == 0) {
-    console.log(`***************: ${selection.rangeCount} is ZERO`)
-    text = $(event.target).text() || $(event.target).parent().text()
-  } else {
-    console.log(`***************: ${selection.rangeCount}`)
-    var clonedSelection = selection.getRangeAt(0).cloneContents();
-    var div = document.createElement('div');
-    div.appendChild(clonedSelection);
-    linksInSelection = $('a[href]', div);
-  }
-
-  if (linksInSelection.length > 0) {
-    console.log(`***************LinksInSelection: ${linksInSelection.length}`)
-
-    links = [];
-    linksInSelection.each(function() {
-      links.push({
-        href: $(this).prop('href'),
-        text: $(this).text(),
-        src:  $(this).find('img').attr('src')
-      });
-    });
-    console.log("here")
-    await chrome.runtime.sendMessage({ action: "copySelection", links: links });
-  } else {
-    await chrome.runtime.sendMessage({ action: "copyElement", text: text });
-  }
-}, true);
 
 var text = ""
 var url = ""
 
 // Listener 1
 chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
-  console.log(`*******************message from background script:  ${message}`)
+  console.log(`*******************message from background script:  ${JSON.stringify(message)}`)
   if (!message.text && !message.url) {
     console.log("Listener 1 received message for action A");
 
+    copy("Try Again")
+    console.log(`********${message.text}*********${message.url}************`)
+
+    // copy("[" + text + "](" + url + ")");
+    // Perform actions specific to action A
+  } else {
+    console.log("Listener 1 received message:", message.text);
     text = message.text;
     url = message.url
 
-    copy("[" + text + "](" + url + ")");
-    // Perform actions specific to action A
-  } else {
-    console.log("Listener 1 received message:", message);
-    copy("[" + text + "]");
+
+    copy(text);
     // Perform default actions for other message types
   }
 });
